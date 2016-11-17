06f62179c1aa600c058d32a3c9ea63c899337045
chaminw <chamin.13@cse.mrt.ac.lk>
2016-11-17 14:50:42 +0530

    initial commit

---
+++ config/module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chamin
+ * Date: 10/31/16
+ * Time: 10:11 AM
+ */
+
+# config/module.php
+
+return  [
+    'modules' => [
+        'Employee',
+        'Job',
+    ]
+];
